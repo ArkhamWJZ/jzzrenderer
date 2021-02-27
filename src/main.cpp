d8b457ff80653558b9063f09f7c3b8ed2223f1c8
万健洲 <2430024291@qq.com>
2021-02-27 20:07:58 +0800

    🛠  import the modules test

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <nanogui/screen.h>
+#include <nanogui/glutil.h>
+#include <nanogui/label.h>
+#include <nanogui/window.h>
+#include <nanogui/layout.h>
+#include <nanogui/entypo.h>
+#include <nanogui/combobox.h>
+#include <nanogui/slider.h>
+#include <nanogui/textbox.h>
+#include <nanogui/checkbox.h>
+#include <nanogui/messagedialog.h>
+#include <iostream>
+
+
+int main(){
+
+    std::cout<<"wan jianzhou"<<std::endl;
+
+    return 0;
+}
