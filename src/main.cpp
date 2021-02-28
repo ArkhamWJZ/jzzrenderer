cbb267aad1118464dfb93259f9b1ec07dcc0f1da
万健洲 <2430024291@qq.com>
2021-02-28 10:03:31 +0800

    🛠  add xml file parser module

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,30 @@
 #include <nanogui/textbox.h>
 #include <nanogui/checkbox.h>
 #include <nanogui/messagedialog.h>
+#include <filesystem/resolver.h>
+#include "pugixml.hpp"
 #include <iostream>
 
+using namespace std;
 
-int main(){
+int main(int argc, char **argv){
 
     std::cout<<"wan jianzhou"<<std::endl;
+
+    std::string sceneName = "";
+    filesystem::path path(argv[1]);
+    try {
+        if (path.extension() == "xml") {
+            sceneName = argv[1];
+            cout << sceneName << endl;
+        } else {
+            cerr << "Fatal error: unknown file \"" << argv[1]
+                 << "\", expected an extension of type .xml" << endl;
+        }
+    } catch (const std::exception &e) {
+        cerr << "Fatal error: " << e.what() << endl;
+        return -1;
+    }
 
     return 0;
 }
